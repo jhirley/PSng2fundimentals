7b72d1f0e75c283f6259c61ea63b13c072a4f753
Jhirley <jhirley.fonte@gmail.com>
2017-02-13 17:33:31 -0500

    Hello World working

---
+++ app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'events-app',
+    templateUrl: 'events-app.component.html'
+})
+export class EventsAppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
