5688d7b2f44056503ce3b3d0d7325ea3389f3dfa
Jhirley <jhirley.fonte@gmail.com>
2017-02-15 19:04:17 -0500

    added navbar.component

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser'
 import {EventsAppComponent } from './events-app.component'
 import {EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
+import { NavBarComponent } from './nav/navbar.component'
+
 
 @NgModule({
     imports: [ BrowserModule],
     declarations: [ 
         EventsAppComponent,
+        NavBarComponent,
         EventsListComponent,
         EventThumbnailComponent
         ],
