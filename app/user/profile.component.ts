feffca24fe3d4dee2077417e05b287d302d29165
Jhirley <jhirley.fonte@gmail.com>
2017-03-06 18:22:27 -0500

    added pattern validator

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -27,7 +27,7 @@ export class ProfileComponent implements OnInit{
   ) { }
 
   ngOnInit() {
-    this.firstName = new FormControl(this.auth.currentUser.firstName, Validators.required)
+    this.firstName = new FormControl(this.auth.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
     this.lastName = new FormControl(this.auth.currentUser.lastName, Validators.required)
 
     this.profileForm = new FormGroup({
