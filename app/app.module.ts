3f534b701d68173c2ca5918709be17f35fd44b9d
Jhirley <jhirley.fonte@gmail.com>
2017-03-15 19:27:40 -0400

    added jQuery to app.module.ts and common/index.barrel

--- app/app.module.ts
+++ app/app.module.ts
@@ -18,20 +18,24 @@ import {
     ,ProperCasePipe
 
  } from './events/index.barrel';
- import { CollapsibleWellComponent } from './common/collapsible-well.component'
 
 import {EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { Error404Component } from './errors/404.component';
 
 // import { ToastrService } from './common/toastr.service'
-import { TOASTR_TOKEN, Toastr } from './common/toastr.service'
+import { TOASTR_TOKEN
+    , Toastr
+    ,JQ_TOKEN
+    ,CollapsibleWellComponent
+} from './common/index.barrel'
 
 import { AuthService } from './user/auth.service';
 
 import { appRoutes } from './routes';
 
 declare let toastr : Toastr;
+declare let jQuery : Object;
 
 @NgModule({
     imports: [ 
