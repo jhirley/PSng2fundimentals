a8e69d5930d839aa04fcb8d71e5c85aa680beb0c
Jhirley <jhirley.fonte@gmail.com>
2017-04-30 16:41:27 -0400

    karma component testing and serices

---
+++ app/events/event-details/session-list.compoent.intergrated.spec.ts
@@ -0,0 +1,17 @@
+import { SessionListComponent } from './session-list.component';
+import { ISession } from '../shared/event.model';
+
+describe('SessionListComponent', () => {
+    let component: SessionListComponent;
+    let mockAuthService, mockVoterService;
+
+    beforeEach(() => {
+        component = new SessionListComponent(mockAuthService, mockVoterService);
+    })
+
+    describe('ngOnChanges', () => {
+        it('should filster the sessions correctly', () => {
+
+        })
+    })
+});
